3b0e43098714ce018fa645c26acfb191892d12c3
Md Sakib <mdaskib5566@gmail.com>
2023-11-11 18:52:14 +0600

    this is updaupgraded version

--- script.js
+++ script.js
@@ -1,66 +1,71 @@
 const upload=document.querySelector(".upload");
-const  fileinput=upload.querySelector("input");
-const previewImg=upload.querySelector("img");
+const takeinp=upload.querySelector("input");
+const previewimg=upload.querySelector("img");
 
-const widthInput=document.querySelector(".width input");
-const heightInput=document.querySelector(".height input")
-const ratioinput=document.querySelector(".ratio input");
+const height=document.querySelector(".height");
+const width=document.querySelector(".width");
 
-
-const download=document.querySelector(".btn");
-const quality=document.querySelector(".quality input");
-
- function loadfile(e){
-    const file=e.target.files[0];
-
-    previewImg.src=URL.createObjectURL(file);
-     
-    previewImg.addEventListener("load",()=>{
-        upload.classList.add("uploadimg");
-
-        widthInput.value=previewImg.naturalWidth;
-        heightInput.value=previewImg.naturalHeight;
-        imgratio= widthInput.value/ heightInput.value;
-    });
- }
-
- widthInput.addEventListener("keyup",()=>{
-    const height=ratioinput.checked?widthInput.value/imgratio : heightInput.value;
-    heightInput.value=Math.floor(height);
- });
-
- heightInput.addEventListener("keyup",()=>{
-    const width=ratioinput.checked?heightInput.value*imgratio : widthInputInput.value;
-    widthInput.value=Math.floor(width);
- });
-
-
-const  convertt=()=>{
-   const canvas=document.createElement("canvas");
-   const ctx=canvas.getContext("2d");
-   canvas.width=widthInput.value;
-   canvas.height=heightInput.value;
-   ctx.drawImage( previewImg,0,0,canvas.width,canvas.height);
-
-
-      const imgquality=quality.checked? 0.3 : 1.0;
-
-
-    const a=document.createElement("a");
-    a.href=canvas.toDataURL("image/jpeg",imgquality);
+function uploadimg(e){
+   file=e.target.files[0];
     
-    a.download=new Date().getTime();
-    
-    a.click();
+   previewimg.src=URL.createObjectURL(file);
+   
+   upload.classList.add("preview");
 
+   width.value=previewimg.naturalWidth;
+   height.value=previewimg.naturalHeight;
+
+   actualratio= width.value/height.value;
 
 }
 
 
-fileinput.addEventListener("change", loadfile);
+width.addEventListener("keyup",()=>{
+const ratio=document.querySelector(".lock-ratio");
+ height.value=Math.floor(ratio.checked? width.value/actualratio : height.value);
+
+});
+
+height.addEventListener("keyup",()=>{
+const ratio=document.querySelector(".lock-ratio");
+ width.value=Math.floor(ratio.checked? height.value*actualratio : width.value);
+
+});
 
 
-upload.addEventListener("click",()=>fileinput.click());
 
 
-download.addEventListener("click",convertt);
+ 
+
+
+
+upload.addEventListener("click",()=>{takeinp.click()});
+
+takeinp.addEventListener("change",uploadimg);
+
+const btn=document.querySelector(".btn");
+
+
+function convert(){
+   const canvas=document.createElement("canvas");
+   const ctx=canvas.getContext("2d");
+   canvas.height=height.value;
+   canvas.width=width.value;
+   
+   ctx.drawImage(previewimg,0,0,   canvas.width,  canvas.height);
+
+     const quality=document.querySelector(".quality");
+     
+    const q= quality.checked? 0.3:1.0;
+   
+   const a= document.createElement("a");
+     a.href=canvas.toDataURL("image/jpeg",q);
+     
+
+     a.download=new Date().getTime();
+     a.click();
+
+}
+
+
+ btn.addEventListener("click",convert);
